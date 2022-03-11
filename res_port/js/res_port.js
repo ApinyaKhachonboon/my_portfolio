77b1cc845904a44e63af11aa59ed3078b1996f28
ApinyaKhachonboon <apinya.khachonboon>
2022-03-11 20:28:26 +0700

    update any css and js

--- res_port/js/res_port.js
+++ res_port/js/res_port.js
@@ -4,14 +4,13 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementsByClassName("myImg");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let isClose = true;
 
 
 for (var i=0;i<img.length; i++) {
     img[i].onclick  = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
         let this_top = this.offsetTop - modal.offsetHeight / 3;
         if(this_top < 0)
         {
@@ -26,6 +25,19 @@ for (var i=0;i<img.length; i++) {
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
+
+window.addEventListener("click", function(event){
+  if(event.target != modalImg && modal.style.display == "block")
+  {
+    if(isClose)
+    {
+      isClose = false;
+      return;
+    }
+    modal.style.display = "none";
+    isClose = true;
+  }
+});
